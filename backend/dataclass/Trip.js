0c9404f91b373dd3493a938d9aba6b61caf6f481
cs20b028-Prakash <cs20b028@iittp.ac.in>
2023-09-17 13:33:07 +0530

    Trip.js updated

--- backend/dataclass/Trip.js
+++ backend/dataclass/Trip.js
@@ -14,10 +14,6 @@ const DataSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  location_name: {
-    type: String,
-    required: true,
-  },
   service_day_name: {
     type: String,
     required: true,
@@ -40,27 +36,6 @@ const DataSchema = new mongoose.Schema({
   },
 }, {
   timestamps: true,
-});
-
-DataSchema.pre('save', async function(next) {
-  const locationName = this.source_name;
-  
-  // set the location name based on the source name
-  this.location_name = locationName;
-  
-  // geocode the location name to obtain its coordinates
-  const result = await geocoder.geocode(locationName);
-  if (result.length > 0) {
-    const { lat, lng } = result[0].geometry.location;
-  
-    // set the longitude and latitude fields in the document
-    this.longitude = lng;
-    this.latitude = lat;
-  } else {
-    console.log(`Unable to geocode location: ${locationName}`);
-  }
-  
-  next();
 });
 
 
